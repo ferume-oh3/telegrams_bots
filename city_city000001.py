11d85ca08c625aaf82f39ef059e042f78465e09f
ferume <ferume.oh.2@gmail.com>
2021-05-18 18:14:43 +0300

    upate

--- city_city000001.py
+++ city_city000001.py
@@ -4,31 +4,24 @@ from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 bot = telebot.TeleBot('1738728282:AAHfH-31hY0DXNlrPmb_JMv9Ao9Quzr5Jfo')
-role = {}
-orders = {}
+us = {}
+
 orders_passengers = {}
 orders_drivers = {}
+finding_right = {}
 
 
-def information(message):
-    if message.chat.id in role and role[message.chat.id] == 0 and message.chat.id in orders_drivers:
-        return len(orders[orders_drivers[message.chat.id]])
-    elif message.chat.id in role and role[message.chat.id] == 1 and message.chat.id in orders_passengers:
-        return len(orders[passenger_order[message.chat.id]])
-    else:
-        return -1
-
-
-def retype_to_string(cnt):
-    if cnt != -1:
-        return "Осталось найти " + str(5 - cnt) + " пассажиров и поедем!"
-    else:
-        return "Мы все потеряли!"
+class user():
+    def __init__(self, role, id, start, finish):
+        self.id = id
+        self.role = role
+        self.start = start
+        self.finish = finish
 
 
 @bot.message_handler(commands=['start'])
 def message_handler_st(message):
-    if message.chat.id not in role:
+    if message.chat.id not in us:
         bot.send_message(message.chat.id, "Привет, я тестовый бот, попробую организовать функционал перевозки пассажиров "
                                       "между городами. \nНе вижу у вас открытых заказов :) \nДля начала определимся водитель ли вы или пассажир?", reply_markup=gen_markup_role())
     else:
@@ -39,7 +32,15 @@ def message_handler_st(message):
 
 @bot.message_handler(commands=['inform'])
 def message_handler_inf(message):
-    bot.send_message(message.chat.id, retype_to_string(information(message)))
+	if message.chat.id not in us:
+		log = (0, "Насколько нам известно у вас отсутсвуют активные заказы!")
+	else:
+		log = is_there((us[message.chat.id].start, us[message.chat.id].finish), message.chat.id)
+	
+	bot.send_message(message.chat.id, log[1])
+	if log[0]:
+		build_trip((us[message.chat.id].start, us[message.chat.id].finish), message)
+
 
 
 @bot.message_handler(content_types=['text'])
@@ -53,11 +54,6 @@ def message_handler(message):
 @bot.message_handler(commands=['information'])
 def message_handler(message):
     information(message)
-
-
-@bot.message_handler(content_types=['text'])
-def message_handler(message):
-    bot.send_message(message.chat.id, "Я вас не понимаю, воспользуйтесь, пожалуйста командой  /start")
 
 
 def gen_markup_role():
@@ -82,36 +78,89 @@ def gen_markup_check_order():
 
 
 def new_order_passenger(message):
-    if message.text.lower() not in orders:
-        msg = bot.send_message(message.chat.id, "Простите, мы не можем зарегистрировать заказ, ни один водитель не берется...")
-        role.pop(message.chat.id, None)
+    msg = bot.send_message(message.chat.id, "Ваш заказ зарегистрирован! Пожалуйста проверяйте статус заказа!")
+    st, fn = convert_to_route(message.text)
+    us[message.chat.id].start = st
+    us[message.chat.id].finish = fn
+    if (st, fn) in orders_drivers:
+    	orders_passengers[(st, fn)].append(us[message.chat.id])
     else:
-        msg = bot.send_message(message.chat.id, "Ваш заказ зарегистрирован! Пожалуйста проверяйте статус заказа!")
-        orders[message.text.lower()].append(message.chat.id)
-        orders_passengers[message.chat.id] = message.text.lower()
+    	orders_passengers[(st, fn)] = [us[message.chat.id]]
 
 
 def new_order_driver(message):
     msg = bot.send_message(message.chat.id, "Ваш заказ зарегистрирован! Пожалуйста проверяйте статус заказа!")
-    orders[message.text.lower()] = [message.chat.id]
-    orders_drivers[message.chat.id] = message.text.lower()
+    st, fn = convert_to_route(message.text)
+    us[message.chat.id].start = st
+    us[message.chat.id].finish = fn
+    if (st, fn) in orders_drivers:
+    	orders_drivers[(st, fn)].append(us[message.chat.id])
+    else:
+    	orders_drivers[(st, fn)] = [us[message.chat.id]]
+
+
+def is_there(route, id):
+	if id in finding_right:
+		return (1, "Поездка состоится!")
+
+	if route not in orders_drivers:
+		return (0, "К сожалению водитель пока не нашелся : (")
+	elif route not in orders_passengers:
+		return (0, "Недостаточно пассажиров, осталось найти 4")
+	elif len(orders_passengers[route]) != 4:
+		return (0, "Недостаточно пассажиров, осталось найти " + str(4 - len(orders_passengers[route])))
+	else:
+		return (1, "Поездка состоится, ура!")
+
+
+def convert_to_route(text):
+	text = text.lower().split()
+	route = (text[0], text[1])
+	return route
+
+
+
+def build_trip(route, message):
+	if message.chat.id in finding_right:
+		msg = bot.send_message(call.message.chat.id, "Необходимые контакты:\n" + '\n'.join(map(str, finding_right[message.chat.id])))
+		finding_right.pop(message.chat.id, None)
+		us.pop(message.chat.id, None)
+	else:	
+		dr = orders_drivers[route][0].id
+		orders_drivers[route].pop(0)
+		ps1 = orders_passengers[route][0].id
+		orders_passengers.pop(0)
+		ps2 = orders_passengers[route][0].id
+		orders_passengers.pop(0)
+		ps3 = orders_passengers[route][0].id
+		orders_passengers.pop(0)
+		ps4 = orders_passengers[route][0].id
+		orders_passengers.pop(0)
+
+		finding_right[dr] = [ps1, ps2, ps3, ps4]
+		finding_right[ps1] = finding_right[ps2] = finding_right[ps3] = finding_right[ps4] = [dr]
+
+
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_query(call):
     if call.data == "cb_driver":
-        role[call.message.chat.id] = 0
+        us[call.message.chat.id] = user(0, call.message.chat.id, "венера", "юпитер")
         bot.answer_callback_query(call.id, "Запомню")
         msg = bot.send_message(call.message.chat.id, "Введите откуда и куда вы хотите ехать, например Казань Уфа")
         bot.register_next_step_handler(msg, new_order_driver)
     elif call.data == "cb_passenger":
-        role[call.message.chat.id] = 1
+        us[call.message.chat.id] = user(1, call.message.chat.id, "юпитер", "венера")
         bot.answer_callback_query(call.id, "Запомню")
         msg = bot.send_message(call.message.chat.id, "Введите откуда и куда вы хотите ехать, например Казань Уфа")
         bot.register_next_step_handler(msg, new_order_passenger)
     elif call.data == "cb_order":
-        inform = retype_to_string(information(call.message))
-        bot.answer_callback_query(call.id, inform)
+        log = is_there((us[message.chat.id].start, us[message.chat.id].finish), message.chat.id)
+        bot.answer_callback_query(call.id, log[1])
+        if log[0]:
+        	build_trip((us[message.chat.id].start, us[message.chat.id].finish), message)
+
     elif call.data == "cb_no_order":
         bot.answer_callback_query(call.id, "Хорошо, приятного тыкания )")
 
